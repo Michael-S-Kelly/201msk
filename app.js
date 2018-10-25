2a654f10a8c490e8a361786e35828dd23a567452
Michael-S-Kelly <mike.s.kelly@icloud.com>
2018-10-24 17:50:45 -0700

    fixed the number guess

--- app.js
+++ app.js
@@ -7,15 +7,19 @@ console.log('visiting should return an undefined value ', visiting);
 console.log('sax should return an undefined value ', sax);
 console.log('car should return an undefined value ', car);
 console.log('dog should return an undefined value ', dog);
+console.log('numberGuess should return an undefined value ', numberGuess);
+console.log('ringName should return an undefined value ', ringName);
 
 // asks a yes or no question question, gives the related variable a stored value, and then checks the consol log to see if the variable stored the input
 
+var score = 0;
 
 var birthState = prompt('Welcome to the site. Do you know if I was born in Washington State? (y/Y/yes/Yes or no...').toLowerCase();
 console.log('birthState should return inputed value ' + birthState);
 
 if (birthState === 'y' || birthState === 'yes'){
   alert('You guessed correctly');
+  score++;
 } else {
   alert('No I was born in Washington State');
 }
@@ -23,10 +27,11 @@ if (birthState === 'y' || birthState === 'yes'){
 var visiting = prompt('Did I live in Japan for 3 years?(y/Y/yes/Yes or no...').toLowerCase();
 console.log('visiting should return inputed value ' + visiting);
 
-if (birthState === 'y' || birthState === 'yes'){
+if (visiting === 'y' || visiting === 'yes'){
   alert('No I lived in South Korea for 3 years');
 } else {
   alert('You are correct, I lived in South Korea for 3 years');
+  score++;
 }
 
 var sax = prompt('Is my favorite Sax player Kenny G?(y/Y/yes/Yes or no...').toLowerCase();
@@ -36,6 +41,7 @@ if (sax === 'y' || sax === 'yes'){
   alert('No, Charlie \"the Bird\" Parker');
 } else {
   alert('You are correct, my favorite sax player is Charlie \"the Bird\" Parker');
+  score++;
 }
 
 var car = prompt('My first car was a Volvo?(y/Y/yes/Yes or no...').toLowerCase();
@@ -43,6 +49,7 @@ console.log('car should return inputed value ' + car);
 
 if (car === 'y' || car === 'yes'){
   alert('Yes, you are correct, my first car was a 1975 Volvo 244GL.');
+  score++;
 } else {
   alert('You are wrong, my first car was a 1975 Volvo 244GL');
 }
@@ -54,6 +61,90 @@ if (dog === 'y' || dog === 'yes'){
   alert('You are wrong, my first dog was named Sarah');
 } else {
   alert('You are right, my first dog was named Sarah');
+  score++;
 }
 
 console.log('dog should return inputed value ' + dog);
+
+var randNumb = 42;
+var numberGuess = parseInt(prompt('I\'m thinking of a number between 1 and 100.  You have 4 guesses to get this one right.'));
+
+
+console.log('numberGuess should return inputed value ' + numberGuess);
+console.log('randNumb should return the value 42 ' + randNumb);
+console.log('i should return the current value ' + i);
+
+var j = 4;
+var i = 1;
+
+
+
+//for(var i = 1; i < 4 || numberGuess === randNumb; i++){
+while(numberGuess !== randNumb && i < 4){
+  
+  if (numberGuess > randNumb){
+    j--;
+    numberGuess = parseInt(prompt('That number is a little high.  Try again, you have ' + j + ' chances.'));
+    console.log('numberGuess should return inputed value ' + numberGuess);
+    console.log('randNumb should return the value 42 ' + randNumb);
+    console.log('i should return the current value ' + i);
+    i++;
+  }  else {
+    j--;
+    numberGuess = parseInt(prompt('That number is a little low.  Try again, you have ' + j + ' chances.'));
+    console.log('numberGuess should return inputed value ' + numberGuess);
+    console.log('randNumb should return the value 42 ' + randNumb);
+    console.log('i should return the current value ' + i);
+    i++;
+  }
+
+
+  // if (numberGuess !== randNumb && i !== 3) {
+  //   if (numberGuess > randNumb){
+  //     j = 4 - i;
+  //     numberGuess = parseInt(prompt('That number is a little high.  Try again, you have ' + j + ' chances.'));
+  //     console.log('numberGuess should return inputed value ' + numberGuess);
+  //     console.log('randNumb should return the value 42 ' + randNumb);
+  //     console.log('i should return the current value ' + i);
+  //   } else {
+  //     j = 4 - i;
+  //     numberGuess = parseInt(prompt('That number is a little high.  Try again, you have ' + j + ' chances.'));
+  //     console.log('numberGuess should return inputed value ' + numberGuess);
+  //     console.log('randNumb should return the value 42 ' + randNumb);
+  //     console.log('i should return the current value ' + i);
+  //   }
+  // }
+}
+
+if (numberGuess == randNumb){
+  alert('Congratz, you have guessed the number.');
+  console.log('numberGuess should return inputed value ' + numberGuess);
+  console.log('randNumb should return the value 42 ' + randNumb);
+  console.log('i should return the current value ' + i);
+  score++;
+
+
+} else {
+  alert('Sorry, you are out of guesses.  The number was ' + randNumb + '.');
+  console.log('numberGuess should return inputed value ' + numberGuess);
+  console.log('randNumb should return the value 42 ' + randNumb);
+  console.log('i should return the current value ' + i);
+}
+
+
+
+
+
+var ringName = prompt('Was my first dog\'s name, Fido?(y/Y/yes/Yes or no...').toLowerCase();
+
+
+if (ringName === 'y' || ringName === 'yes'){
+  alert('You are wrong, my first dog was named Sarah');
+} else {
+  alert('You are right, my first dog was named Sarah');
+  score++;
+}
+
+console.log('ringName should return inputed value ' + ringName);
+
+alert('You have gotten ' + score + ' correct out of 7.');
